f49d0ba12e7b6cbc5f341d40b46a6f247c01e34b
Chiranjeevi6 <chiranjeeviankaiah6@gmail.com>
2023-10-30 16:08:40 -0500

    add

--- routes/mobile.js
+++ routes/mobile.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('fall', { title: 'Search Results Fall' });
+  res.render('mobile', { title: 'Search Results Fall' });
 });
+
 
 module.exports = router;
